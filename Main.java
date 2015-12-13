9b92153d44f8104acd634bafbb1063c7657affb8
Andriy Zasypkin <Andy29485@users.noreply.github.com>
2015-12-13 16:08:33 -0500

    Implemented solution

--- Main.java
+++ Main.java
@@ -1,10 +1,27 @@
-//TODO: <Name>
-//TODO: <Date>
+//Andriy Zasypkin
+//2015-12-13
 //Practice 2012 - 01: Grading Exams
 
 public class Main {
   public static void main(String[] args) {
+    //create an input reader object
+    BufferedReader input
+            = new BufferedReader(new InputStreamReader(System.in));
 
+    int nCases = Integer.valueOf(input.readLine());
+    for(int i=1; i <= nCases; i++) {
+      int nIncorrect = 0;
+      int nQuestions = Integer.valueOf(input.readLine());
+      String student = input.readLine();
+      String key = input.readLine();
+      for(int j=0; j<nQuestions; j++) {
+        if(student.charAt(j) != key.charAt(j)) nIncorrect++;
+      }
+      System.out.printf("Case %d: %d\n", i, nIncorrect);
+    }
+
+    //close stream
+    input.close();
     System.exit(0);
   }
 }
